cced74914a31f873638941b3c8c50023b824e951
Ben Keating <ben@abstractly.io>
2021-07-25 14:49:39 -0500

    ability to detect correct answer and add points

--- src/screens/QuestionScreen.svelte
+++ src/screens/QuestionScreen.svelte
@@ -1,7 +1,10 @@
 <script>
-  // The question payload comes in by way of a component prop.
+  // The question payload and it's handler come in by way of a component prop.
   export let question;
   export let handleNextQuestion;
+
+  // We keep track of the current question verdict ephemirally in local state.
+  let verdict;
 
   import {
     currentScreen,
@@ -12,9 +15,7 @@
     currentQuestion,
   } from '../store';
 
-  const handleShowResults = () => {
-    currentScreen.set(2);
-  };
+  const handleShowResults = () => currentScreen.set(2);
 
   const handleUseLifeLine = (slug) => {
     if (slug === '5050') {
@@ -39,6 +40,25 @@
   $: sat8 = $totalScore > 10000000 ? 'is-past' : '';
   $: sat9 = $totalScore > 100000000 ? 'is-past' : '';
   $: sat10 = $totalScore > 100000000 ? 'is-past' : '';
+
+  let answers = question.incorrect_answers;
+  answers.push(question.correct_answer);
+
+  let shuffledAnswers = answers.sort(() => 0.5 - Math.random());
+
+  // let buttonElList = document.querySelectorAll("input[type=button]");
+
+  const handleSelectAnswer = (e, answer) => {
+    e.preventDefault();
+    if (answer === question.correct_answer) {
+      console.log('CORRECT!');
+      totalScore.update(n => n + question.points);
+      verdict = 'correct';
+    } else {
+      console.log('incorrect');
+      verdict = 'incorrect';
+    }
+  };
 </script>
 
 <link href="/styles.css" rel="stylesheet" type="text/css" />
@@ -57,61 +77,68 @@
       <li class="{sat2} transition-all">10 sats</li>
       <li class="{sat1} transition-all">1 sat</li>
     </ul>
-    <div class="content">
-      <div class="question">
-        {question.id}: {question.question}
-      </div>
-      <ul class="answers">
-        <li class="answer">
-          <button>One</button>
-        </li>
-        <li class="answer">
-          <button disabled="disabled">Two</button>
-        </li>
-        <li class="answer">
-          <button class="is-error">Three</button>
-        </li>
-        <li class="answer">
-          <button class="is-success" on:click={() => totalScore.update(s => s + question.points)}>Four</button>
-        </li>
-      </ul>
-    </div>
-    <div class="footer">
-      <div class="score">
-        <div>
-          <input type="range" bind:value={$totalScore} max="10000000000" class="block w-full"  />
-          <div class="block">{$totalScore} sats</div>
+
+    {#if verdict === 'correct'}
+      <div class="content correct-content">
+        CORRECT! <br /> {question.points} sats for youuuu.
+        <div class="mt-12 actions is-visible">
+          <button class="px-12 py-2 bg-blue-500" on:click={() => handleNextQuestion(question.id)}>Next</button>
         </div>
       </div>
-
-      <div class="lifelines is-visible">
-        <button
-          on:click={() => lifelinePhoneEnabled.set(false)}
-          disabled={!$lifelinePhoneEnabled}
-          class="btn-lifeline {!$lifelinePhoneEnabled && 'opacity-50 cursor'}"
-        >
-          <img src="./images/phone-a-friend.png" />
-        </button>
-        <button
-          on:click={() => lifeline5050Enabled.set(false)}
-          disabled={!$lifeline5050Enabled}
-          class="btn-lifeline {!$lifeline5050Enabled && 'opacity-50 cursor'}"
-        >
-          <img src="./images/50-50.png" />
-        </button>
-        <button
-          on:click={() => lifelineAudienceEnabled.set(false)}
-          disabled={!$lifelineAudienceEnabled}
-          class="btn-lifeline {$lifelineAudienceEnabled ? 'opacity-100' : 'opacity-50 cursor'}"
-        >
-          <img src="./images/audience.webp" />
-        </button>
+    {:else if verdict === 'incorrect'}
+      <div class="content incorrect-content">
+        D'oh. Sorry, but you just missed out on {question.points} sats.
+        <div class="mt-12 actions is-visible">
+          <button class="px-12 py-2 bg-red-900" on:click={() => handleNextQuestion(question.id)}>Next</button>
+        </div>
       </div>
-
-      <div class="actions is-visible">
-        <button on:click={() => handleNextQuestion(question.id)}>Next</button>
+    {:else}
+      <div class="content">
+        <div class="question">
+          {question.id}: {question.question}
+          <p class="text-sm">{question.points} sats</p>
+        </div>
+        <ul class="answers">
+          {#each shuffledAnswers as answer}
+            <li class="answer">
+              <button on:click={(e) => handleSelectAnswer(e, answer)}>{answer}</button>
+            </li>
+          {/each}
+        </ul>
       </div>
-    </div>
+      <div class="footer">
+        <div class="score">
+          <div>
+            <input type="range" bind:value={$totalScore} max="10000000000" class="block w-full" />
+            <div class="block">{$totalScore} sats</div>
+          </div>
+        </div>
+
+        <div class="lifelines is-visible">
+          <button
+            on:click={() => lifelinePhoneEnabled.set(false)}
+            disabled={!$lifelinePhoneEnabled}
+            class="btn-lifeline {!$lifelinePhoneEnabled && 'opacity-50 cursor'}"
+          >
+            <img src="./images/phone-a-friend.png" />
+          </button>
+          <button
+            on:click={() => lifeline5050Enabled.set(false)}
+            disabled={!$lifeline5050Enabled}
+            class="btn-lifeline {!$lifeline5050Enabled && 'opacity-50 cursor'}"
+          >
+            <img src="./images/50-50.png" />
+          </button>
+          <button
+            on:click={() => lifelineAudienceEnabled.set(false)}
+            disabled={!$lifelineAudienceEnabled}
+            class="btn-lifeline {$lifelineAudienceEnabled ? 'opacity-100' : 'opacity-50 cursor'}"
+          >
+            <img src="./images/audience.webp" />
+          </button>
+        </div>
+      </div>
+    {/if}
   </div>
 {/if}
 
@@ -121,5 +148,11 @@
   }
   .btn-lifeline:active {
     @apply bg-transparent;
+  }
+  .correct-content {
+    @apply text-4xl text-center text-green-500;
+  }
+  .incorrect-content {
+    @apply text-4xl text-center text-red-500;
   }
 </style>
