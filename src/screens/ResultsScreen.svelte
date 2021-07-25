f9e1ca30d7b4cb4f40bc966112165689246754b0
Ben Keating <ben@abstractly.io>
2021-07-25 14:56:00 -0500

    results screen shows total

--- src/screens/ResultsScreen.svelte
+++ src/screens/ResultsScreen.svelte
@@ -1,5 +1,15 @@
+<script>
+  import { totalScore } from '../store';
+</script>
+
 <link href="/styles.css" rel="stylesheet" type="text/css" />
 <div class="screen parent" style="background-image: url('./images/raining.gif');">
-  <img class="center-results image1" src="https://media.giphy.com/media/VsIvr9QjJJK2eQiCSf/giphy.gif" alt="scrouge"  height="100%" />
+  <div class="absolute" style="z-index: 9999; color: red; font-size: 400%;">Your score: {$totalScore}</div>
+  <img
+    class="center-results image1"
+    src="https://media.giphy.com/media/VsIvr9QjJJK2eQiCSf/giphy.gif"
+    alt="scrouge"
+    height="100%"
+  />
   <img class="center-results image2 center center-winner" src="./images/Winner.gif" alt="winner" />
 </div>
